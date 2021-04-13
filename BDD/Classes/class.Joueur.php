c54aa7dcba6547bb7447b1230f08d6a7a6c7a42e
BONIN <raphael.bonin@edu.univ-fcomte.fr>
2021-04-13 12:35:27 +0200

    modification page Connexion.html en connexioninscription.html Ajout typographie.css Correction  de différentes pages
    Création connexion.php et inscription.php

--- BDD/Classes/class.Joueur.php
+++ BDD/Classes/class.Joueur.php
@@ -2,32 +2,56 @@
 
 class Joueur {
     public $id = 0;
+    public $pseudo = null;
     public $nom = null;
     public $prenom = null;
-    public $image = null;
-    public $adressemail = null;
     public $datenaissance = 0;
-    public $categoriepref = null;
+    public $adressemail = null;
+    public $mdp = null;
+    public $confirmationmdp = null;
 
     /**
      * Joueur constructor.
      * @param int $id
+     * @param null $pseudo
      * @param null $nom
      * @param null $prenom
-     * @param null $image
-     * @param null $adressemail
      * @param int $datenaissance
-     * @param null $categoriepref
+     * @param null $adressemail
+     * @param null $mdp
+     * @param null $confirmationmdp
      */
-    public function __construct($id, $nom, $prenom, $image, $adressemail, $datenaissance, $categoriepref)
+    public function __construct($id, $pseudo, $nom, $prenom, $datenaissance, $adressemail, $mdp, $confirmationmdp)
     {
         $this->id = $id;
+        $this->pseudo = $pseudo;
         $this->nom = $nom;
         $this->prenom = $prenom;
-        $this->image = $image;
-        $this->adressemail = $adressemail;
         $this->datenaissance = $datenaissance;
-        $this->categoriepref = $categoriepref;
+        $this->adressemail = $adressemail;
+        $this->mdp = $mdp;
+        $this->confirmationmdp = $confirmationmdp;
     }
 
+
+    public function inscription(){
+        echo '3';
+        $pdo = new PDO('mysql:host=localhost;port=8889;dbname=quaeludo','root','root');
+        echo '4';
+        $req = $pdo->prepare("INSERT INTO JOUEUR(Pseudo, NOM, Prenom, DateNaissance, AdresseMail, Mdp, ConfirmationMdp) VALUES (:pseudo, :nom, :prenom, :datenaissance, :adressemail, :mdp, :confirmationmdp)");
+        echo '5';
+        $req->execute(
+            array(
+                ":pseudo" => $this->pseudo,
+                ":nom" => $this->nom,
+                ":prenom" => $this->prenom,
+                ":datenaissance" => $this->datenaissance,
+                "adressemail" => $this->adressemail,
+                ":mdp" => $this->mdp,
+                ":confirmationmdp" => $this->confirmationmdp
+            )
+        );
+        echo '6';
+
+    }
 }
--- Test.php
+++ Test.php
@@ -9,9 +9,9 @@
 </head>
 <body>
     <?php require_once 'BDD/Classes/class.Groupe.php';
-    ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+                        ini_set('display_errors', 1);
+                        ini_set('display_startup_errors', 1);
+                        error_reporting(E_ALL);
 ?>
 
     <div>
